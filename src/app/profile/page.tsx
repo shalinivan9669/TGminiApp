ca2292b056209968384c69c63589bd5962cd6e79
shalinivan9669 <77225504+shalinivan9669@users.noreply.github.com>
2024-11-11 21:17:34 +0600

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { useAppContext } from '../context/AppContext';
-import CharacterCard from '@/components/CharacterCard';
 
 const Profile: React.FC = () => {
   const { user, setUser } = useAppContext();
@@ -12,14 +11,30 @@ const Profile: React.FC = () => {
     setUser(null);
   };
 
+  const handleLogin = () => {
+    // Пример логики входа пользователя
+    setUser({
+      id: 'user123',
+      telegramId: 987654321,
+      username: 'Имя пользователя',
+      imageUrl: '/images/user-avatar.jpg', // Опционально
+    });
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow p-4">
         <h1 className="text-2xl font-bold mb-4">Профиль пользователя</h1>
         {user ? (
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <img src="/images/user-avatar.jpg" alt="Аватар пользователя" className="w-24 h-24 rounded-full mb-4" />
-            <h2 className="text-xl font-semibold mb-2">{user}</h2>
+            {/* Отображение аватара пользователя */}
+            <img
+              src={user.imageUrl || "/images/default-avatar.jpg"}
+              alt="Аватар пользователя"
+              className="w-24 h-24 rounded-full mb-4"
+            />
+            {/* Отображение имени пользователя */}
+            <h2 className="text-xl font-semibold mb-2">{user.username}</h2>
             <p className="text-gray-600 mb-4">Описание профиля пользователя.</p>
             <button
               onClick={handleLogout}
@@ -32,7 +47,7 @@ const Profile: React.FC = () => {
           <div className="bg-white p-6 rounded-lg shadow-md">
             <h2 className="text-xl font-semibold mb-2">Вы не вошли в систему</h2>
             <button
-              onClick={() => setUser('Имя пользователя')}
+              onClick={handleLogin}
               className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
             >
               Войти
